70867340896185c9b36da267e3983921b51c3716
Daniel Adeoluwa Ademoye <danielademoye3@gmail.com>
2025-10-06 14:16:08 -0700

    Delete phone_home.py

--- phone_home.py
+++
@@ -1,7 +0,0 @@
-def send_message(route):
-    times = []
-    for i in route:
-        times.append(((i/300000) + 0.5))
-    return round(((sum(times))-0.5), 4)
-print(send_message([802101, 725994, 112808, 3625770, 481239]))
-
